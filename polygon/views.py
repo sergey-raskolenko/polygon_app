b41bdc2a526e4fc58a0f2653995807625868fb7e
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-04-12 22:24:46 +0300

    changed view

--- polygon/views.py
+++ polygon/views.py
@@ -1,35 +1,38 @@
-
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 
 from polygon.forms import PolygonForm
-from polygon.models import Polygon
+from polygon.models import PolygonModel
+from django.contrib.gis.geos import Polygon
 
 
 class PolygonCreateView(CreateView):
-    model = Polygon
+    model = PolygonModel
     form_class = PolygonForm
-    success_url = reverse_lazy('polygon_list')
+    success_url = reverse_lazy('polygon:polygon_list')
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Неверные данные')
+        return super().form_invalid(form)
 
     def form_valid(self, form):
         polygon = form.save(commit=False)
-        latitude_1 = form.cleaned_data['latitude_1']
-        longitude_1 = form.cleaned_data['longitude_1']
-        latitude_2 = form.cleaned_data['latitude_2']
-        longitude_2 = form.cleaned_data['longitude_2']
-
-        if longitude_1 > 180:
-            form.cleaned_data['longitude_1'] = longitude_1 - 360
-            form.cleaned_data['intersects_antimeridian'] = True
-        if longitude_2 > 180:
-            form.cleaned_data['longitude_2'] = longitude_2 - 360
-            form.cleaned_data['intersects_antimeridian'] = True
-
+        print(polygon.__dict__)
+        print(form.cleaned_data)
+        intersects_antimeridian = False
+        for i in range(4):
+            if form.cleaned_data[f'longitude_{i + 1}'] > 180:
+                form.cleaned_data[f'longitude_{i + 1}'] -= 360
+                intersects_antimeridian = True
+        polygon.intersects_antimeridian = intersects_antimeridian
+        polygon.polygon = Polygon([
+            (form.cleaned_data[f'latitude_{i+1}'], form.cleaned_data[f'longitude_{i+1}']) for i in range(4)
+        ])
         polygon.save()
-
         return super().form_valid(form)
 
 
 class PolygonListView(ListView):
-    model = Polygon
+    model = PolygonModel
     context_object_name = 'polygons'

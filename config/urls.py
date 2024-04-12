6d5593fc2358c0beebeba6883f9456a2ce678012
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-04-12 22:28:08 +0300

    simple changes

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('polygon_app.urls', namespace='polygon')),
+    path('', include('polygon.urls', namespace='polygon')),
 ]

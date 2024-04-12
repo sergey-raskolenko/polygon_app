4ee88ecbec8b39bde5be239495ce97b747aac65b
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-04-12 22:25:00 +0300

    changed model

--- polygon/models.py
+++ polygon/models.py
@@ -1,10 +1,10 @@
 from django.contrib.gis.db import models
 
 
-class Polygon(models.Model):
+class PolygonModel(models.Model):
     name = models.CharField(max_length=100)
-    polygon = models.PolygonField()
-    intersects_antimeridian = models.BooleanField(default=False)
+    polygon = models.PolygonField(null=True, blank=True)
+    intersects_antimeridian = models.BooleanField(null=True, blank=True)
 
     def __str__(self):
         return self.name
